11757400684cf9c370aae244076fb3aedeeade60
Ivan Salmin <salmin.iv@gmail.com>
2019-12-18 17:18:12 +0400

    Adds mocks to the project

---
+++ src/const.js
@@ -0,0 +1,14 @@
+const COLORS = [`black`, `yellow`, `blue`, `green`, `pink`];
+
+const FILTER_NAMES = [`all`, `overdue`, `today`, `favorites`, `repeating`, `tags`, `archive`];
+
+const DAYS = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
+
+const MONTH_NAMES = [`January`, `February`, `March`, `April`, `May`, `June`, `July`, `August`, `September`, `October`, `November`, `December`];
+
+export {
+  COLORS,
+  FILTER_NAMES,
+  DAYS,
+  MONTH_NAMES,
+};
